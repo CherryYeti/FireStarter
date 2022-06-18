747fdf725137cd2da20b00ad59122a49b440ac29
CherryYeti <jasonjavandel@gmail.com>
2022-06-17 23:15:50 -0700

    fixed spacing

--- deploy-command.js
+++ deploy-command.js
@@ -12,9 +12,7 @@ const commands = [
 	new SlashCommandBuilder().setName('who').setDescription('Lists who is on the corresponding server').addIntegerOption(option =>option.setName('server_number').setDescription('Server number').setRequired(true))
 ]
 	.map(command => command.toJSON());
-
 const rest = new REST({ version: '9' }).setToken(token);
-
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
