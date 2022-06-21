11a68dd30fa775bf2733af50420563e174505213
CherryYeti <jasonjavandel@gmail.com>
2022-06-20 22:33:36 -0700

    changed quotes to single

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('interactionCreate', async interaction => {
     } else if (commandName === 'start') {
         var selected = interaction.options.getInteger('server_number');
         if (numberOutOfRange(selected)) {
-            await interaction.reply(selected.toString() + " is not a server!");
+            await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             var server = servers_data.servers[selected];
             var java_path = servers_data.versions[server.ver];
@@ -28,44 +28,44 @@ client.on('interactionCreate', async interaction => {
             var path = server.path;
             await interaction.reply(`Starting server ${name}`);
             processes[selected] = child_process.exec(`cd ${dir} && ${java_path} -Xms${minimum_ram}M -Xmx${maximum_ram}M -jar ${path} nogui`);
-            processes[selected].stdout.on("data", (data) => {processOutput(data, interaction, selected)});
+            processes[selected].stdout.on('data', (data) => {processOutput(data, interaction, selected)});
         }
     } else if (commandName === 'cmd') {
         var selected = interaction.options.getInteger('server_number');
         var command = interaction.options.getString('command');
         if (numberOutOfRange(selected)) {
-            await interaction.reply(selected.toString() + " is not a server!");
+            await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
-                await interaction.reply("Executed command " + command);
+                await interaction.reply('Executed command ' + command);
                 processes[selected].stdin.write(command + '\n');
             } else {
-                await interaction.reply("This server is currently not running");
+                await interaction.reply('This server is currently not running');
             }
         }
     } else if (commandName === 'who') {
         var selected = interaction.options.getInteger('server_number');
         if (numberOutOfRange(selected)) {
-            await interaction.reply(selected.toString() + " is not a server!");
+            await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
-                await interaction.reply("Listing");
+                await interaction.reply('Listing');
                 processes[selected].stdin.write('/list\n');
             } else {
-                await interaction.reply("This server is currently not running");
+                await interaction.reply('This server is currently not running');
             }
         }
     } else if (commandName === 'stop') {
         var selected = interaction.options.getInteger('server_number');
         if (numberOutOfRange(selected)) {
-            await interaction.reply(selected.toString() + " is not a server!");
+            await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
-                await interaction.reply("Stopping server " + selected.toString());
+                await interaction.reply('Stopping server ' + selected.toString());
                 processes[selected].stdin.write('stop\n');
                 running[selected] = false;
             } else {
-                await interaction.reply("This server is currently not running");
+                await interaction.reply('This server is currently not running');
             }
         }
     }
@@ -78,19 +78,19 @@ function numberOutOfRange(selected){
     }
 }
 async function processOutput(data, interaction, selected) {
-    if (data.includes("For help, type ")) {
-        await interaction.followUp("Server is now up!");
+    if (data.includes('For help, type ')) {
+        await interaction.followUp('Server is now up!');
         running[selected] = true;
-    } else if (data.includes("players online:")) {
-        await interaction.followUp("Online " + data.toString().substring(data.indexOf(":", 70)) + "\n");
-    } else if (data.includes("joined the game")) {
-        await interaction.followUp(data.toString().substring(data.indexOf(":", 30) + 2) + "\n");
-    } else if (data.includes("left the game")) {
-        await interaction.followUp(data.toString().substring(data.indexOf(":", 30) + 2) + "\n");
-    } else if (data.includes("<")) {
-        await interaction.followUp(data.toString().substring(data.indexOf("<", 30)) + "\n");
-    } else if (data.includes("EULA")) {
-        await interaction.followUp("You must accept the EULA for server " + selected + " before you can start it");
+    } else if (data.includes('players online:')) {
+        await interaction.followUp('Online ' + data.toString().substring(data.indexOf(':', 70)) + '\n');
+    } else if (data.includes('joined the game')) {
+        await interaction.followUp(data.toString().substring(data.indexOf(':', 30) + 2) + '\n');
+    } else if (data.includes('left the game')) {
+        await interaction.followUp(data.toString().substring(data.indexOf(':', 30) + 2) + '\n');
+    } else if (data.includes('<')) {
+        await interaction.followUp(data.toString().substring(data.indexOf('<', 30)) + '\n');
+    } else if (data.includes('EULA')) {
+        await interaction.followUp('You must accept the EULA for server ' + selected + ' before you can start it');
         running[selected] = false;
     }
 }
@@ -101,25 +101,25 @@ function getPath(pathString) {
 }
 function printServers() {
     var message = ''
-    for (server in servers_data["servers"]) {
-        message = message.concat(`[${server}] ${servers_data["servers"][server].name} | Online ${running[server]} \n`);
+    for (server in servers_data['servers']) {
+        message = message.concat(`[${server}] ${servers_data['servers'][server].name} | Online ${running[server]} \n`);
     }
     const serverEmbed = new discord.MessageEmbed()
-        .setColor("#F9734E")
-        .setTitle("Servers")
+        .setColor('#F9734E')
+        .setTitle('Servers')
         .setDescription(message);
     return serverEmbed;
 }
 const helpEmbed = new discord.MessageEmbed()
-    .setColor("#F9734E")
-    .setTitle("HELP ME")
-    .setDescription("List of commands")
+    .setColor('#F9734E')
+    .setTitle('HELP ME')
+    .setDescription('List of commands')
     .addFields(
-        { name: "/help", value: "What brought you here" },
-        { name: "/servers", value: "Lists all available servers" },
-        { name: "/start (server number)", value: "Starts server with corresponding value" },
-        { name: "/stop (server number)", value: "Stops server with corresponding value" },
-        { name: "/cmd (server number) (command)", value: "Execute command with corresponding server (NO SLASHES)" },
-        { name: "/who (server number)", value: "Lists who is on the corresponding server" }
-    ).setFooter("Made by CherryYeti", "https://i.ibb.co/TKLgBhh/unnamed.jpg");
+        { name: '/help', value: 'What brought you here' },
+        { name: '/servers', value: 'Lists all available servers' },
+        { name: '/start (server number)', value: 'Starts server with corresponding value' },
+        { name: '/stop (server number)', value: 'Stops server with corresponding value' },
+        { name: '/cmd (server number) (command)', value: 'Execute command with corresponding server (NO SLASHES)' },
+        { name: '/who (server number)', value: 'Lists who is on the corresponding server' }
+    ).setFooter('Made by CherryYeti', 'https://i.ibb.co/TKLgBhh/unnamed.jpg');
 client.login(token);
