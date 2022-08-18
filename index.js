8f83b731cdb20e70cb9204e9f48afb5c995557a5
CherryYeti <jasonjavandel@gmail.com>
2022-08-18 23:07:56 +0000

    Changed variable structure to make it consistent

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const { token } = require('./config.json');
 const child_process = require('child_process');
 const discord = require('discord.js');
-const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 const fs = require('fs');
+const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
 const servers_data = JSON.parse(fs.readFileSync('servers.json'));
 var processes = new Array(servers_data.servers.length);
 var running = new Array(servers_data.servers.length).fill(false);
-//Allows the bot to sign in
+//Allows the bot to sign in`
 client.once('ready', () => { console.log('Ready to work!'); });
 //Runs whenever the bot receives a command
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
-    const { commandName } = interaction;
-    if (commandName === 'help') {
-        await interaction.reply({ embeds: [helpEmbed] });
-    } else if (commandName === 'servers') {
-        await interaction.reply({ embeds: [printServers()] });
-    } else if (commandName === 'start') {
+    const { command_name } = interaction;
+    if (command_name === 'help') {
+        await interaction.reply({ embeds: [help_embed] });
+    } else if (command_name === 'servers') {
+        await interaction.reply({ embeds: [print_servers()] });
+    } else if (command_name === 'start') {
         //Get command paramaters
         var selected = interaction.options.getInteger('server_number');
-        if (numberOutOfRange(selected)) {
+        if (number_out_of_range(selected)) {
             await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             //Grab settings from servers.json file
@@ -28,32 +28,32 @@ client.on('interactionCreate', async interaction => {
             var minimum_ram = server.minram;
             var maximum_ram = server.maxram;
             var name = server.name;
-            var dir = getPath(server.path);
+            var dir = get_path(server.path);
             var path = server.path;
             await interaction.reply(`Starting server ${name}`);
             //Start the server and save it as an array element
             processes[selected] = child_process.exec(`cd ${dir} && ${java_path} -Xms${minimum_ram}M -Xmx${maximum_ram}M -jar ${path} nogui`);
             //Pipe output data to a function
-            processes[selected].stdout.on('data', (data) => { processOutput(data, interaction, selected) });
+            processes[selected].stdout.on('data', (data) => { process_output(data, interaction, selected) });
         }
-    } else if (commandName === 'cmd') {
+    } else if (command_name === 'cmd') {
         //Get command paramaters
         var selected = interaction.options.getInteger('server_number');
         var command = interaction.options.getString('command');
-        if (numberOutOfRange(selected)) {
+        if (number_out_of_range(selected)) {
             await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
-                await interaction.reply('Executed command ' + command);
                 processes[selected].stdin.write(command + '\n');
+                await interaction.reply('Executed command ' + command);
             } else {
                 await interaction.reply('This server is currently not running');
             }
         }
-    } else if (commandName === 'who') {
+    } else if (command_name === 'who') {
         //Get command paramaters
         var selected = interaction.options.getInteger('server_number');
-        if (numberOutOfRange(selected)) {
+        if (number_out_of_range(selected)) {
             await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
@@ -63,9 +63,9 @@ client.on('interactionCreate', async interaction => {
                 await interaction.reply('This server is currently not running');
             }
         }
-    } else if (commandName === 'stop') {
+    } else if (command_name === 'stop') {
         var selected = interaction.options.getInteger('server_number');
-        if (numberOutOfRange(selected)) {
+        if (number_out_of_range(selected)) {
             await interaction.reply(selected.toString() + ' is not a server!');
         } else {
             if (processes[selected]) {
@@ -78,7 +78,7 @@ client.on('interactionCreate', async interaction => {
         }
     }
 });
-function numberOutOfRange(selected) {
+function number_out_of_range(selected) {
     //Checks if sever paramater in command is valid
     if (selected >= servers_data.servers.length | selected < 0) {
         return true;
@@ -86,7 +86,7 @@ function numberOutOfRange(selected) {
         return false;
     }
 }
-async function processOutput(data, interaction, selected) {
+async function process_output(data, interaction, selected) {
     //Dictates responses to certain keywords in server output
     if (data.includes('For help, type ')) {
         await interaction.followUp('Server is now up!');
@@ -104,15 +104,15 @@ async function processOutput(data, interaction, selected) {
         running[selected] = false;
     }
 }
-function getPath(pathString) {
+function get_path(path_string) {
     //Grab path to change to before launching server
-    var last = pathString.lastIndexOf('/');
-    var path = pathString.substring(0, last + 1);
+    var last = path_string.lastIndexOf('/');
+    var path = path_string.substring(0, last + 1);
     return path;
 }
-function printServers() {
+function print_servers() {
     //Lists all servers
-    if(servers_data>0){
+    if (servers_data > 0) {
         var message = ''
         for (server in servers_data['servers']) {
             message = message.concat(`[${server}] ${servers_data['servers'][server].name} | Online ${running[server]} \n`);
@@ -131,9 +131,9 @@ function printServers() {
         }
         return noServersEmbed;
     }
-    
+
 }
-const helpEmbed = {
+const help_embed = {
     color: 0xF9734E,
     title: 'HELP ME',
     description: 'List of commands',
