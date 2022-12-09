a220d11ef0da399281d5f1e4a612213c7a754dee
CherryYeti <jasonjavandel@gmail.com>
2022-12-08 20:58:18 -0800

    Big update! Everything should work again.

--- README.md
+++ README.md
@@ -1,7 +1,184 @@
-# Welcome to FireStarter
+<h1 align="center">FireStarter</h1>
 
-FireStarter is a free and open source Discord bot used to start, stop and manage options on your minecraft servers using Node.js
+<p align="center">
+  <img src="images/favicon.png" alt="angular-logo" width="120px" height="120px"/>
+  <br>
+    <i>
+      FireStarter is a free and open source Discord bot used to start, stop and manage options on your minecraft servers using Node.js
+    </i>
+  <br>
+</p>
 
-Check out the [website](https://fire-starter-website.vercel.app/) to get started!
+<p align="center">
+  <a href="https://javandel.net/jason">My Portfolio</a>
+  ·
+  <a href="https://github.com/cherryyeti/firestarter/issues">Submit an Issue</a>
+  <br>
+  <br>
+</p>
 
-Firestarter uses the [DiscordJS api](https://discord.js.org/#/) to act as a medium between your system and discord.
+- [Prerequisites](#prerequisites)
+  - [Node JS (Node.js 16.9.0 or newer is required!)](#node-js-nodejs-1690-or-newer-is-required)
+    - [Windows](#windows)
+    - [MacOS](#macos)
+    - [Linux](#linux)
+  - [Java](#java)
+    - [Windows](#windows-1)
+    - [MacOS](#macos-1)
+    - [Linux](#linux-1)
+- [Downloading the code](#downloading-the-code)
+- [Configuring the code](#configuring-the-code)
+  - [Java.ini file](#javaini-file)
+    - [Windows](#windows-2)
+    - [MacOS (Untested paths)](#macos-untested-paths)
+    - [Linux](#linux-2)
+  - [Servers.ini file](#serversini-file)
+- [Setting up the discord bot (little hard)](#setting-up-the-discord-bot-little-hard)
+- [Final commands!](#final-commands)
+- [The commands](#the-commands)
+- [Feedback/Issues](#feedbackissues)
+
+# Prerequisites
+
+## Node JS (Node.js 16.9.0 or newer is required!)
+
+### Windows
+
+- Visit the [Node.js] website and download either the current or lts version
+
+### MacOS
+  
+- Visit the [Node.js] website and download either the current or lts version
+
+### Linux
+
+- Check [this website](https://nodejs.org/en/download/package-manager/) for how to install it with your package manager
+
+## Java
+
+You need to install two different version of java in order to play all the versions of minecraft.
+
+- For minecraft version 1.0 to version 1.16.5, you need to have Java 8 installed
+- For minecraft version 1.17 to the current version, you need to have Java 17 installed
+
+### Windows
+
+- Visit the Adtoptium Website and download both [Java 17] and [Java 8]
+
+### MacOS
+
+- Visit the Adtoptium Website and download both [Java 17] and [Java 8]
+
+### Linux
+
+- Download openjdk 8 and 17 from your repository using your package manager
+
+
+# Downloading the code
+
+- It is recommended that you download the code by pressing the code button at the top of this page
+
+# Configuring the code
+
+This may require some poking around, but it isn't super difficult
+
+The two files that you need to be concerned with are <i>java.ini</i> and <i>servers.ini</i>
+
+<i>java.ini</i> contains the file paths to your java installations
+
+<i>servers.ini</i> contains the file paths to your server jar files as well as options for those jar files
+
+## Java.ini file
+
+Inside of the java.ini file, there will be two entries, <strong>8</strong> and <strong>17</strong>
+
+You need to fill these paths with the location of your java installations
+
+### Windows
+
+Because windows is a hellish operating system, their folder path are different from everyone else.
+The paths for the files are located at `C:\Program Files\Eclipse Adoptium\jdk-[version]-hotspot\bin\java.exe`
+
+When you paste these java paths into the java.ini file, <strong>MAKE SURE TO ADD TWO BACKSLASHES INSTEAD OF ONE (I.E. `C:\Program Files` -> `C:\\Program Files`</strong>
+
+### MacOS (Untested paths)
+
+- I believe that the path to the java installations should be
+
+`/usr/local/opt/openjdk@8/bin/java`
+
+`/usr/local/opt/openjdk@17/bin/java`
+
+### Linux
+
+- For most distrobutions that I've used, these are the paths
+
+`/usr/lib/jvm/java-8-openjdk/jre/bin/java`
+
+`/usr/lib/jvm/java-17-openjdk/jre/bin/java`
+
+## Servers.ini file
+
+This file should be pretty straight forward
+
+`[Vanilla]` is simply an id to determine seperate servers, if possible, please do not name these sections the same
+
+The `PATH` variable should be a direct path to the server jar file.
+When you paste server jar paths into the servers.ini file, <strong>MAKE SURE TO ADD TWO BACKSLASHES INSTEAD OF ONE (I.E. `C:\Program Files` -> `C:\\Program Files`</strong>
+
+The `JAVA` variable is the version of java that the server should run on. Refer to [this section](#java) to see which version to use. (Value should be either 8 or 17)
+
+`MIN_RAM` represents the minimum amount of ram that your server will use in Megabytes (i.e. 4Gb ram = 4096Mb)
+
+`MIN_RAM` represents the maximum amount of ram that your server will use in Megabytes (i.e. 4Gb ram = 4096Mb)
+
+The `NAME` variable represents what the /servers command will print. This name is not used for anything else.
+
+# Setting up the discord bot (little hard)
+
+1. The first thing that you need to do is visit your [Discord Developer Page]
+2. Click on the `New Application` button located in the top right corner
+3. Name the bot whatever you want (preferrably FireStarter) and tick TOS box below and the create button
+4. This will bring you to the General Information page of your application. In the middle of the page, there is a section that says `APPLICATION ID`, click on the copy button below the ID and paste it into the .env file in the `ID` variable.
+5. On the left side of the screen, on the navbar, there will be a button named Bot. Click on the button
+6. Click on the add bot button that pops up
+7. Under the profile picture of the bot, there will be a section named Authorization Flow. <strong>MAKE SURE TO TURN OFF THE PUBLIC BOT OPTION.</strong>
+8. Further down the page, there will be a section named Privileged Gateway Intents, turn on all of the options and click the green Save Changes button that appears at the bottom of the screen
+9. Back to the top of the page, click on the button that says reset token, this will grab the token that your discord bot uses to communicate with discord. <strong>DO NOT EVER SHARE THIS TOKEN WITH ANYONE, NO MATTER WHAT</strong>
+10. Click the copy token button below the token and paste it into the .env file in the `TOKEN` variable
+11. On the left navbar, there will be a dropdown labeled OAuth2. Click it and select the URL Generator option
+12. In the scopes section, select both bot, and applications.commands
+13. Press the copy button next to the url at the bottom of the page and paste it in a new tab
+14. Select which server you want to add it to
+15. Go the server you just added to bot to and copy the server id. (discord.com/channels/`SERVERID`/dontcopythislastpart)
+16. Paste the server id into the .env file in the `GUILD` variable
+17. That's it! The bot will handle registering the commands, and if everything is set up correctly, you should be good to go!
+
+# Final commands!
+
+Use the command prompt or terminal to navigate to the folder that firestarter is installed in, and type `npm i`, this will install all of the packages that allows your bot to communicate with discord.
+
+Type `node index.js` to run the bot!
+
+# The commands
+
+The `/help` command will list all of the commands
+
+The `/servers` command will list all of the server listed in the servers.ini file
+
+The `/start` command will start the server with the corresponding value. (Values can be found by running the `/servers` command)
+
+The `/stop` command will stop the server with the corresponding value. (Values can be found by running the `/servers` command)
+
+The `/cmd` command will send a command the server with the corresponding value. (Values can be found by running the `/servers` command) (No slash is needed in the cmd option of the `/cmd` command)
+
+The `/who` command will list all active platers on the server with the corresponding value. (Values can be found by running the `/servers` command)
+
+# Feedback/Issues
+
+Any and all feedback is greatly appreciated, as this is my first real project that I've seen to completion. Thank you!
+
+[Discord Developer Page]: https://discord.com/developers/applications
+[Java 17]: https://adoptium.net/temurin/releases/?version=17
+[Java 8]: https://adoptium.net/temurin/releases/?version=8
+[node.js]: https://nodejs.org/
--- index.js
+++ index.js
@@ -1,197 +1,278 @@
-const { token } = require('./config.json')
-const child_process = require('child_process')
-const discord = require('discord.js')
-const fs = require('fs')
+//Imports
+const child_process = require("child_process");
+const discord = require("discord.js");
+const dotenv = require("dotenv");
+const fs = require("fs");
+const ini = require("ini");
+const path = require("path");
+const os = require('os');
+//Load token and server from .env file
+dotenv.config();
+
+//Grab java information from INI file
+const java_config = ini.parse(fs.readFileSync("./java.ini", "utf-8"));
+const java_config_keys = Reflect.ownKeys(java_config);
+
+//Grab server information from INI file
+const server_config = ini.parse(fs.readFileSync("./servers.ini", "utf-8"));
+const server_config_keys = Reflect.ownKeys(server_config);
+
+//Create empty arrays for reference
+const server_processes = new Array(server_config_keys.length);
+const server_running = new Array(server_config_keys.length).fill(false);
+
+//Initialize discord client
 const client = new discord.Client({
   intents: [discord.GatewayIntentBits.Guilds],
-})
-const serversData = JSON.parse(fs.readFileSync('servers.json'))
-var processes = new Array(serversData.servers.length)
-var running = new Array(serversData.servers.length).fill(false)
-//Runs once the bot logs in
-client.once('ready', () => {
-  console.log('FireStarter v2.0.5 ----- Built on 9/13/22 \nReady to work!')
-})
-//Runs whenever the bot receives a command
-client.on('interactionCreate', async (interaction) => {
-  var selected = interaction.options.getInteger('server_number')
-  if (!interaction.isChatInputCommand()) return
-  const { commandName } = interaction
-  if (commandName === 'help') {
-    await interaction.reply({ embeds: [helpEmbed] })
-  } else if (commandName === 'servers') {
-    await interaction.reply({ embeds: [printServers()] })
-  } else if (commandName === 'start') {
-    //Get command paramaters
-    if (numberOutOfRange(selected)) {
-      await interaction.reply(selected.toString() + ' is not a server!')
+});
+
+//Client prints once it is connected
+client.once("ready", () => {
+  console.log("FireStarter v2.1.0 ----- Built on 12/08/22 \nReady to work!");
+  //   deployCommands();
+});
+
+client.on("interactionCreate", async (interaction) => {
+  //Checks if the commands is a slash command
+  if (!interaction.isChatInputCommand()) return;
+  //Get associated slash command number option if applicable
+  var selected = interaction.options.getInteger("server_number");
+  const { commandName } = interaction;
+  //Help command
+  if (commandName == "help") {
+    await interaction.reply({ embeds: [help_embed] });
+  //Lists all servers in the server.ini file
+  } else if (commandName == "servers") {
+    console.log("SERVER");
+    await interaction.reply({ embeds: [print_servers()] });
+  //Grabs info from servers.ini and java.ini to execute command to launch server
+  } else if (commandName == "start") {
+    //Checks if given slash command number is a vaild server
+    if (selected < server_config_keys.length && 0 <= selected) {
+      var selected_server_data =
+        server_config[server_config_keys[selected].toString()];
+      var jar_path = selected_server_data["PATH"];
+      var folder = path.dirname(jar_path);
+      var ver = selected_server_data["JAVA"];
+      var java = java_config["JAVA"][ver];
+      var min_ram = selected_server_data["MIN_RAM"];
+      var max_ram = selected_server_data["MAX_RAM"];
+      var name = selected_server_data["NAME"];
+      //Put parenthesis around paths in windows because mircosoft sucks
+      if (os.platform()=='win32'){
+        jar_path = `'${jar_path}'`
+        java = `'${java}'`
+      }
+      server_processes[selected] = child_process.exec(
+        `cd ${folder} && ${java} -Xms${min_ram}M -Xmx${max_ram}M -jar ${jar_path} nogui`
+      );
+
+      server_processes[selected].stdout.on("data", (data) => {
+        process_server_output(data, interaction, selected);
+      });
+
+      await interaction.reply(
+        `Server ${selected} (${name}) is being started...`
+      );
+      server_running[selected] = true;
     } else {
-      //Grab settings from servers.json file
-      var server = serversData.servers[selected]
-      var javaPath = serversData.versions[server.ver]
-      var minimumRam = server.minram
-      var maximumRam = server.maxram
-      var name = server.name
-      var dir = getPath(server.path)
-      var path = server.path
-      await interaction.reply(`Starting server ${name}`)
-      //Start the server and save it as an array element
-      processes[selected] = child_process.exec(
-        `cd ${dir} && ${javaPath} -Xms${minimumRam}M -Xmx${maximumRam}M -jar ${path} nogui`
-      )
-      //Pipe output data to a function
-      processes[selected].stdout.on('data', (data) => {
-        processOutput(data, interaction, selected)
-      })
+      await interaction.reply(`${selected.toString()} is not a server!`);
     }
-  } else if (commandName === 'cmd') {
-    //Get command paramaters
-    var command = interaction.options.getString('command')
-    if (numberOutOfRange(selected)) {
-      await interaction.reply(selected.toString() + ' is not a server!')
+    //Stops server based on slash command number
+  } else if (commandName == "stop") {
+    if (selected < server_config_keys.length && 0 <= selected) {
+      await interaction.reply(`Stopping server ${selected.toString()}`);
+      server_processes[selected].stdin.write("stop\n");
+      server_running[selected] = false;
     } else {
-      if (processes[selected]) {
-        processes[selected].stdin.write(command + '\n')
-        await interaction.reply('Executed command ' + command)
+      await interaction.reply(`${selected.toString()} is not a server!`);
+    }
+    //Sends command to server based on slash command number
+  } else if (commandName == "cmd") {
+    if (selected < server_config_keys.length && 0 <= selected) {
+      var command = interaction.options.getString("command");
+      if (server_processes[selected]) {
+        server_processes[selected].stdin.write(`${command}\n`);
+        await interaction.reply(`Executed command ${command}`);
       } else {
-        await interaction.reply('This server is currently not running')
+        await interaction.reply("This server is currently not running");
       }
     }
-  } else if (commandName === 'who') {
-    //Get command paramaters
-    if (numberOutOfRange(selected)) {
-      await interaction.reply(selected.toString() + ' is not a server!')
+    //Lists all people on selected server
+  } else if (commandName == "who") {
+    if (server_processes[selected]) {
+      server_processes[selected].stdin.write(`/list\n`);
     } else {
-      if (processes[selected]) {
-        await interaction.reply('Listing')
-        processes[selected].stdin.write('/list\n')
-      } else {
-        await interaction.reply('This server is currently not running')
-      }
-    }
-  } else if (commandName === 'stop') {
-    if (numberOutOfRange(selected)) {
-      await interaction.reply(selected.toString() + ' is not a server!')
-    } else {
-      if (processes[selected]) {
-        await interaction.reply('Stopping server ' + selected.toString())
-        processes[selected].stdin.write('stop\n')
-        running[selected] = false
-      } else {
-        await interaction.reply('This server is currently not running')
-      }
+      await interaction.reply("This server is currently not running");
     }
   }
-})
-function numberOutOfRange(selected) {
-  //Checks if sever paramater in command is valid
-  if ((selected >= serversData.servers.length) | (selected < 0)) {
-    return true
-  } else {
-    return false
+});
+//Function that handles the output of the server command
+async function process_server_output(data, interaction, selected) {
+  if (data.includes("For help, type ")) {
+    await interaction.followUp("Server is now up!");
+    server_running[selected] = true;
+  } else if (data.includes("players online:")) {
+    await interaction.followUp(
+      `Online ${data.toString().substring(data.indexOf(":", 70))}\n`
+    );
+  } else if (data.includes("joined the game")) {
+    // await interaction.followUp(
+    //   data.toString().substring(data.indexOf(":", 30) + 2) + "\n"
+    // );
+  } else if (data.includes("left the game")) {
+    // await interaction.followUp(
+    //   data.toString().substring(data.indexOf(":", 30) + 2) + "\n"
+    // );
+  } else if (data.includes("<")) {
+    // await interaction.followUp(
+    //   data.toString().substring(data.indexOf("<", 30)) + "\n"
+    // );
+  } else if (data.includes("EULA")) {
+    await interaction.followUp(
+      `You must accept the EULA for server ${selected} before you can start it`
+    );
+    server_running[selected] = false;
   }
 }
-async function processOutput(data, interaction, selected) {
-  //Dictates responses to certain keywords in server output
-  if (data.includes('For help, type ')) {
-    await interaction.followUp('Server is now up!')
-    running[selected] = true
-  } else if (data.includes('players online:')) {
-    await interaction.followUp(
-      'Online ' + data.toString().substring(data.indexOf(':', 70)) + '\n'
-    )
-  } else if (data.includes('joined the game')) {
-    await interaction.followUp(
-      data.toString().substring(data.indexOf(':', 30) + 2) + '\n'
-    )
-  } else if (data.includes('left the game')) {
-    await interaction.followUp(
-      data.toString().substring(data.indexOf(':', 30) + 2) + '\n'
-    )
-  } else if (data.includes('<')) {
-    await interaction.followUp(
-      data.toString().substring(data.indexOf('<', 30)) + '\n'
-    )
-  } else if (data.includes('EULA')) {
-    await interaction.followUp(
-      'You must accept the EULA for server ' +
-        selected +
-        ' before you can start it'
-    )
-    running[selected] = false
-  }
-}
-function getPath(pathString) {
-  //Grab path to change to before launching server
-  var last = pathString.lastIndexOf('/')
-  var path = pathString.substring(0, last + 1)
-  return path
-}
-function printServers() {
-  //Lists all servers
-  if (serversData.servers.length > 0) {
-    var message = ''
-    for (var server in serversData['servers']) {
+
+function print_servers() {
+  if (server_config_keys.length > 0) {
+    var message = "";
+    for (let key in server_config_keys) {
       message = message.concat(
-        `[${server}] ${serversData['servers'][server].name} | Online ${running[server]} \n`
-      )
+        `${
+          server_config[server_config_keys[key].toString()]["NAME"]
+        } (Server ${key}) | Online ${server_running[key]}\n`
+      );
     }
     const serverEmbed = {
       color: 0xf9734e,
-      title: 'Servers',
+      title: "Servers",
       description: message,
       footer: {
-        text: 'Made by CherryYeti',
-        icon_url: 'https://i.ibb.co/DC1GF9z/favicon.png',
+        text: "Made by CherryYeti",
+        icon_url: "https://i.ibb.co/DC1GF9z/favicon.png",
       },
-    }
-    return serverEmbed
+    };
+    return serverEmbed;
   } else {
     const noServersEmbed = {
       color: 0xf9734e,
-      title: 'Servers',
-      description: 'There are currently no servers added to firestarter',
+      title: "Servers",
+      description: "There are currently no servers added to firestarter",
       footer: {
-        text: 'Made by CherryYeti',
-        icon_url: 'https://i.ibb.co/DC1GF9z/favicon.png',
+        text: "Made by CherryYeti",
+        icon_url: "https://i.ibb.co/DC1GF9z/favicon.png",
       },
-    }
-    return noServersEmbed
+    };
+    return noServersEmbed;
   }
 }
-const helpEmbed = {
+
+const help_embed = {
   color: 0xf9734e,
-  title: 'HELP ME',
-  description: 'List of commands',
+  title: "HELP ME",
+  description: "List of commands",
   fields: [
     {
-      name: '/help',
-      value: 'What brought you here',
+      name: "/help",
+      value: "What brought you here",
     },
     {
-      name: '/servers',
-      value: 'Lists all available servers',
+      name: "/servers",
+      value: "Lists all available servers",
     },
     {
-      name: '/start',
-      value: 'Starts server with corresponding value',
+      name: "/start",
+      value: "Starts server with corresponding value",
     },
     {
-      name: '/stop',
-      value: 'Stops server with corresponding value',
+      name: "/stop",
+      value: "Stops server with corresponding value",
     },
     {
-      name: '/cmd',
-      value: 'Execute command with corresponding server (NO SLASHES)',
+      name: "/cmd",
+      value: "Execute command with corresponding server (NO SLASHES)",
     },
     {
-      name: '/who',
-      value: 'Lists who is on the corresponding server',
+      name: "/who",
+      value: "Lists who is on the corresponding server",
     },
   ],
   footer: {
-    text: 'Made by CherryYeti',
-    icon_url: 'https://i.ibb.co/DC1GF9z/favicon.png',
+    text: "Made by CherryYeti",
+    icon_url: "https://i.ibb.co/DC1GF9z/favicon.png",
   },
+};
+//Deploys slash commands to discord
+function deployCommands() {
+  const commands = [
+    new discord.SlashCommandBuilder()
+      .setName("help")
+      .setDescription("Get started"),
+    new discord.SlashCommandBuilder()
+      .setName("servers")
+      .setDescription("Lists all available servers"),
+    new discord.SlashCommandBuilder()
+      .setName("start")
+      .setDescription("Starts server with corresponding value")
+      .addIntegerOption((option) =>
+        option
+          .setName("server_number")
+          .setDescription("Server number")
+          .setRequired(true)
+      ),
+    new discord.SlashCommandBuilder()
+      .setName("stop")
+      .setDescription("Stops server with corresponding value")
+      .addIntegerOption((option) =>
+        option
+          .setName("server_number")
+          .setDescription("Server number")
+          .setRequired(true)
+      ),
+    new discord.SlashCommandBuilder()
+      .setName("cmd")
+      .setDescription("Execute command with corresponding server")
+      .addIntegerOption((option) =>
+        option
+          .setName("server_number")
+          .setDescription("Server number")
+          .setRequired(true)
+      )
+      .addStringOption((option) =>
+        option
+          .setName("command")
+          .setDescription("Command to execute")
+          .setRequired(true)
+      ),
+    new discord.SlashCommandBuilder()
+      .setName("who")
+      .setDescription("Lists who is on the corresponding server")
+      .addIntegerOption((option) =>
+        option
+          .setName("server_number")
+          .setDescription("Server number")
+          .setRequired(true)
+      ),
+  ].map((command) => command.toJSON());
+  if (process.env.TOKEN && process.env.ID && process.env.GUILD) {
+    const rest = new discord.REST({ version: "9" }).setToken(process.env.TOKEN);
+    rest
+      .put(
+        discord.Routes.applicationGuildCommands(
+          process.env.ID,
+          process.env.GUILD
+        ),
+        {
+          body: commands,
+        }
+      )
+      .then(() => console.log("Successfully registered application commands."))
+      .catch(console.error);
+  }
 }
-client.login(token)
+
+// Start the client
+client.login(process.env.TOKEN);
